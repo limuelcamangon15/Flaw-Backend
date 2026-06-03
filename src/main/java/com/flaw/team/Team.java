656961f06af1873a8b0eeae65f7d2762fba9cf00
limuelcamangon15 <limuelmartincamangon@gmail.com>
2026-06-04 00:13:53 +0800

    update: team entity

--- src/main/java/com/flaw/team/Team.java
+++ src/main/java/com/flaw/team/Team.java
@@ -1,4 +1,65 @@
 package com.flaw.team;
 
+import com.flaw.user.User;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Team {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "created_by")
+    private User createdBy;
+
+    @ManyToMany
+    @JoinTable(
+            name = "tean_members",
+            joinColumns = @JoinColumn(name = "team_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
+    private List<User> members = new ArrayList<>();
+
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public User getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(User createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public List<User> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<User> members) {
+        this.members = members;
+    }
+
 }
