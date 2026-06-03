f7bf2fd95b254e3125f83cc50d72dbafc00ff056
limuelcamangon15 <limuelmartincamangon@gmail.com>
2026-06-03 15:25:59 +0800

    first commit

---
+++ src/main/java/com/flaw/FlawApplication.java
@@ -0,0 +1,13 @@
+package com.flaw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlawApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flaw/FlawApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flaw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlawApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
