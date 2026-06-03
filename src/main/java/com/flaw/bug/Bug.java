43d9856d7974840798bdcfae94a4762b143bc7f8
limuelcamangon15 <limuelmartincamangon@gmail.com>
2026-06-04 00:35:51 +0800

    update: entity bug

--- src/main/java/com/flaw/bug/Bug.java
+++ src/main/java/com/flaw/bug/Bug.java
@@ -1,4 +1,47 @@
 package com.flaw.bug;
 
+import com.flaw.team.Team;
+import com.flaw.user.User;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "bugs")
 public class Bug {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private BugStatus status = BugStatus.OPEN;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private BugCategory category;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "reporter_id")
+    private User reporter;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "asignee_id")
+    private User asignee;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt =  LocalDateTime.now();
+
 }
