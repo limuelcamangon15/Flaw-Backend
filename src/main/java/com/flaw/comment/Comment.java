4211115e66b323cd31ccf6b70c55002f9b4bf0a7
limuelcamangon15 <limuelmartincamangon@gmail.com>
2026-06-04 00:44:26 +0800

    update: entity comments

--- src/main/java/com/flaw/comment/Comment.java
+++ src/main/java/com/flaw/comment/Comment.java
@@ -1,4 +1,33 @@
 package com.flaw.comment;
 
+import com.flaw.bug.Bug;
+import com.flaw.user.User;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import org.springframework.cglib.core.Local;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "comments")
 public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "bug_id")
+    private Bug bug;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "author_id")
+    private User author;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
 }
