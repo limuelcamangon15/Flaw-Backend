c1276704ffda7fbc5f82225f935f46127101a876
limuelcamangon15 <limuelmartincamangon@gmail.com>
2026-06-04 22:53:34 +0800

    update: finished controller layer of comments

---
+++ src/main/java/com/flaw/comment/CommentController.java
@@ -0,0 +1,35 @@
+package com.flaw.comment;
+
+import com.flaw.utils.ApiResponse;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/comments")
+public class CommentController {
+    private final CommentService commentService;
+
+    public CommentController(CommentService commentService){
+        this.commentService = commentService;
+    }
+
+    @PostMapping
+    public ResponseEntity<CommentResponse> createComment(@Valid @RequestBody CommentRequest request){
+        return ResponseEntity.ok(commentService.addComment(request));
+    }
+
+    @GetMapping("/bug/{bugId}")
+    public ResponseEntity<List<CommentResponse>> getCommentsByBug(@RequestParam Long bugId){
+        return ResponseEntity.ok(commentService.getCommentsByBug(bugId));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> deleteComment(@RequestParam Long id){
+        commentService.deleteComment(id);
+
+        return ResponseEntity.ok(new ApiResponse<>(true, "Comment deleted successfully!",null) );
+    }
+}
