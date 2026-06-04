dcd1eb45dab5711585a2eb9412828fe139950b2d
limuelcamangon15 <limuelmartincamangon@gmail.com>
2026-06-04 22:32:07 +0800

    update: added add comment on comment repo

--- src/main/java/com/flaw/comment/CommentService.java
+++ src/main/java/com/flaw/comment/CommentService.java
@@ -1,4 +1,38 @@
 package com.flaw.comment;
 
+import com.flaw.auth.AuthUtil;
+import com.flaw.bug.Bug;
+import com.flaw.bug.BugRepository;
+import com.flaw.user.User;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CommentService {
+    private final CommentRepository commentRepository;
+    private final BugRepository bugRepository;
+    private final AuthUtil authUtil;
+
+    public CommentService(CommentRepository commentRepository, BugRepository bugRepository, AuthUtil authUtil) {
+        this.commentRepository = commentRepository;
+        this.bugRepository = bugRepository;
+        this.authUtil = authUtil;
+    }
+
+    // Add comment
+    public CommentResponse addComment(CommentRequest request){
+        User author = authUtil.getCurrentUser();
+
+        Bug bug = bugRepository.findById(request.bugId)
+                .orElseThrow(() -> new RuntimeException("Bug not found"));
+
+        Comment comment = new Comment();
+        comment.setAuthor(author);
+        comment.setContent(request.content);
+        comment.setBug(bug);
+
+        // save
+        commentRepository.save(comment);
+
+        return CommentResponse.from(comment);
+    }
 }
