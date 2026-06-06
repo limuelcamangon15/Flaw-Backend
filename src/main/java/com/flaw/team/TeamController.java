b5c142fac3f3959beadfbcda5b8cf10a4aaadbf4
limuelcamangon15 <limuelmartincamangon@gmail.com>
2026-06-06 16:49:10 +0800

    update: implemented custom response body on team controller

--- src/main/java/com/flaw/team/TeamController.java
+++ src/main/java/com/flaw/team/TeamController.java
@@ -1,5 +1,6 @@
 package com.flaw.team;
 
+import com.flaw.utils.ApiResponse;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -19,23 +20,39 @@ public class TeamController {
 
     @PostMapping
     @PreAuthorize("hasRole('QA')")
-    public ResponseEntity<TeamResponse> createTeam(@Valid @RequestBody TeamRequest request){
-        return ResponseEntity.ok(teamService.createTeam(request));
+    public ResponseEntity<ApiResponse<TeamResponse>> createTeam(@Valid @RequestBody TeamRequest request){
+        return ResponseEntity.ok(new ApiResponse<>(
+                true,
+                "success",
+                teamService.createTeam(request)
+        ));
     }
 
     @PostMapping("/{teamId}/members/{userId}")
     @PreAuthorize("hasRole('QA')")
-    public ResponseEntity<TeamResponse> addMember(@PathVariable Long teamId, @PathVariable Long userId){
-        return ResponseEntity.ok(teamService.addMember(teamId, userId));
+    public ResponseEntity<ApiResponse<TeamResponse>> addMember(@PathVariable Long teamId, @PathVariable Long userId){
+        return ResponseEntity.ok(new ApiResponse<>(
+                true,
+                "success",
+                teamService.addMember(teamId, userId)
+        ));
     }
 
     @GetMapping("/{teamId}")
-    public ResponseEntity<TeamResponse> getTeam(@PathVariable Long teamId){
-        return ResponseEntity.ok(teamService.getTeam(teamId));
+    public ResponseEntity<ApiResponse<TeamResponse>> getTeam(@PathVariable Long teamId){
+        return ResponseEntity.ok(new ApiResponse<>(
+                true,
+                "success",
+                teamService.getTeam(teamId)
+        ));
     }
 
     @GetMapping("/my")
-    public ResponseEntity<List<TeamResponse>> getMyTeams(){
-        return ResponseEntity.ok(teamService.getMyTeams());
+    public ResponseEntity<ApiResponse<List<TeamResponse>>> getMyTeams(){
+        return ResponseEntity.ok(new ApiResponse<>(
+                true,
+                "success",
+                teamService.getMyTeams()
+        ));
     }
 }
